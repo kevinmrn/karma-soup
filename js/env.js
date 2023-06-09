a2147f6674124b3eb1315f26ed7bf25c910f7eef
kevinmrn <88911299+kevinmrn@users.noreply.github.com>
2023-06-09 21:53:20 +0100

    Update env.js

--- js/env.js
+++ js/env.js
@@ -1,4 +1,4 @@
-const nftAddress = ""; // Paste the address of the NFT collection.
+const nftAddress = "0x6B37fC6250D467A91365ce8da678FedBbe00375C"; // Paste the address of the NFT collection.
 // uncomment the chain you are using:
 const chainId = "0x507"; // Moonbase
 // const chainId = "0x504"; // Moonbeam
